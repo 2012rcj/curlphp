cf4a312763e83d49279ab35d079959b3d6e75cdc
99rstreet <2012rcj@gmail.com>
2017-03-15 12:07:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+<?php
+$params=['name'=>'John', 'surname'=>'Doe', 'age'=>36)
+$defaults = array(
+CURLOPT_URL => 'http://myremoteservice/', 
+CURLOPT_POST => true,
+CURLOPT_POSTFIELDS => $params,
+);
+$ch = curl_init();
+curl_setopt_array($ch, ($options + $defaults));
+?>
